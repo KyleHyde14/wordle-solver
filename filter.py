b1f3fb6e25f1ddc893b1b4a91197aa91835d5177
jesushdez <jesushdezjob@gmail.com>
2025-05-06 10:44:21 +0200

    filtered-in words without special chars

--- filter.py
+++ filter.py
@@ -1,10 +1,12 @@
 file = open('spanish_len_5.txt', 'r')
+abc = 'abcdefghijklmnñopqrstuvwxyz'
 
 def write_list_to_file(txt, py_file):
     with open(py_file, 'w') as f:
         f.write(f"words = [\n")
         for line in txt:
-            f.write(f"  '{line.strip()}', \n")
+            if all(x in abc for x in line.strip()):
+                f.write(f"  '{line.strip()}', \n")
 
         f.write(f"]\n")
 
